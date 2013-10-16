9a67c4ed56a390b2723f9c58ff25e12a949563be
Stefano Torresi <webdev@stefanotorresi.it>
2013-10-16 11:30:40 +0200

    update string wrapper interface

--- library/Zend/Stdlib/StringWrapper/StringWrapperInterface.php
+++ library/Zend/Stdlib/StringWrapper/StringWrapperInterface.php
@@ -79,6 +79,16 @@ interface StringWrapperInterface
     public function strpos($haystack, $needle, $offset = 0);
 
     /**
+     * Find the position of the last occurrence of a substring in a string
+     *
+     * @param string $haystack
+     * @param string $needle
+     * @param int    $offset
+     * @return int|false
+     */
+    public function strrpos($haystack, $needle, $offset = 0);
+
+    /**
      * Convert a string from defined encoding to the defined convert encoding
      *
      * @param string  $str
